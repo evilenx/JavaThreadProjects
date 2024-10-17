26e298955f0c7bebd6086dcc11c5c88bec5b4537
evilenx <evilenx@proton.me>
2024-10-16 20:16:31 -0500

    add enable/disable buttons in Print gui

--- Asig01/PrintsNumbersGui1.1/PrintsNumbers.java
+++ Asig01/PrintsNumbersGui1.1/PrintsNumbers.java
@@ -1,3 +1,7 @@
+/**
+ * * @author: Emanuel Avilés (Ec-25-15187)
+ *   Date: 15-10-24 (sustentación)
+ */
 import javax.swing.*;
 
 public class PrintsNumbers implements Runnable {
@@ -51,52 +55,3 @@ public class PrintsNumbers implements Runnable {
         }
     }
 }
-
-
-
-
-
-
-//import javax.swing.*;
-//
-//public class PrintsNumbers implements Runnable {
-//  private boolean keepGoing;
-//  private int sleepTime;
-//  private JTextArea outputArea;
-//
-//  public PrintsNumbers(int sleepTime, JTextArea outputArea) {
-//    this.keepGoing = true;
-//    this.sleepTime = sleepTime;
-//    this.outputArea = outputArea;
-//  }
-//
-//  public void stopPrinting() {
-//    this.keepGoing = false;
-//  }
-//
-//  @Override
-//    public void run() {
-//      int number = 1;
-//      while (keepGoing) {
-//        if (number > 10)
-//        {/*Si el numero ya se pasa de 10, debes detener el hilo*/
-//          stopPrinting();
-//          outputArea.append("Thread.interrupt();\n");
-//        }
-//        else {
-//          /*
-//           * Si el numero no es mayor de diez, sumarle uno a number y duermes el hilo
-//           * Imprimir en el JTextArea
-//           */
-//          outputArea.append(number++ + "\n");
-//          try
-//          {/*Dormir por el tiempo especificado*/
-//            Thread.sleep(sleepTime);
-//          }
-//          catch (InterruptedException e) {
-//            Thread.currentThread().interrupt(); // restaurar estado de interrupción
-//          }
-//        }
-//      }
-//    }
-//}
